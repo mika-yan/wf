28d7810c3d89a8cc939ea9bafedae9f13179c492
mika-yan <kryuger.mikaelyan@inbox.ru>
2020-06-22 20:56:38 +0300

    Updates

---
+++ precache-manifest.5399b17aa6c3eee8d385f5d9c9263244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02108f1331b48824d3b01f478bc0ed3a",
+    "url": "/wf/index.html"
+  },
+  {
+    "revision": "8993af8a03452f071d55",
+    "url": "/wf/static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "8993af8a03452f071d55",
+    "url": "/wf/static/js/2.1c060a34.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/wf/static/js/2.1c060a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7700758ec59f79fba11",
+    "url": "/wf/static/js/main.00943492.chunk.js"
+  },
+  {
+    "revision": "7098df4d51b955ac5e3b",
+    "url": "/wf/static/js/runtime-main.ac8072d1.js"
+  }
+]);
